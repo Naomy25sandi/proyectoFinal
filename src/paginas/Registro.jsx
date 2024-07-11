e67d058136e98b6c0cdcb3a5a7794406730d7a05
Naomy25sandi <naomysandi25@gmail.com>
2024-07-11 09:37:58 -0600

    comente el registro

--- src/paginas/Registro.jsx
+++ src/paginas/Registro.jsx
@@ -11,19 +11,19 @@ const Registro = () => {
   const [usuario, setUsuario] = useState()
   const [correo, setCorreo] = useState()
   const [contra, setContra] = useState()
-  const nombreRef = useRef("")
-  const emailRef = useRef("")
-  const passwordRef = useRef("")
+  const nombreRef = useRef("") //
+  const emailRef = useRef("") //
+  const passwordRef = useRef("") //
   const [data, setData] = useState([])
   const navigate = useNavigate()
   const [msg, setMsg] = useState("");
-  // llamo la funcion de posdata 
-
+  
+  //funcion asyncronica para enviar datos a la API
   const darData = async (obj) => {
-    const datos = await postData(obj, "users")
+    const datos = await postData(obj, "users")// llamo la funcion de posdata 
     setData(datos)
   }
-  // llamo la funcion de get data con el usseffect
+  // llamo la funcion de get data con el usseffect para se que ejecute una sola vez
   useEffect(() => {
     const obtenerUsuarios = async () => {
       const dataUsuarios = await GetData("users")
@@ -32,33 +32,36 @@ const Registro = () => {
     }
     obtenerUsuarios()
   }, []);
+ 
   // validaciones para los espacios en blanco
   const validaUsuario = async () => {
     const nombreTrim = nombreRef.current.value.trim()
     const emailTrim = emailRef.current.value.trim()
     const passwordTrim = passwordRef.current.value.trim()
 
+    //Verificar si el correo electrónico ya está registrado
     const user = data.find((user) => user.email === correo);
     if (user) {
-      Swal.fire({
+      Swal.fire({// envio swissalert
         icon: "error",
         title: "Usuario existente",
         text: "El correo electronico ya esta registrado"
       });
       navigate("/") // lo direcciono a login ya tiene cuenta
     } else {
-      if (!nombreTrim || !emailTrim || !passwordTrim) {
+      if (!nombreTrim || !emailTrim || !passwordTrim) {// Validar que todos los campos estén llenos
         Swal.fire({
           icon: "error",
           text: "Llene todos los campos"
         })
       } else {
+         // Envia datos del nuevo usuario a la API
         darData({
           name: usuario,
           email: correo,
           password: contra
         })
-
+        // hook para dirigirme a home si todo salio bien
         navigate("/")
         setMsg("Usuario registrado existosamente!")
       }
